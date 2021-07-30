d5fc540008e840e82860f658ed7269c115550c90
Kaytotes <me@kaytotes.online>
2021-07-30 17:48:12 +0100

    Localisation languages

---
+++ DBC/Languages.cs
@@ -0,0 +1,22 @@
+namespace dbc_export
+{
+    enum Languages
+    {
+        enUS,
+        enGB,
+        koKR,
+        frFR,
+        deDE,
+        enCN,
+        zhCN,
+        enTW,
+        zhTW,
+        esES,
+        esMX,
+        ruRU,
+        ptPT,
+        ptBR,
+        itIT,
+        Unk,
+    }
+}
